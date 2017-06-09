0e6489d7930337bafc51571bf93e6eb308e22c6f
Tarun <gtarun1991@gmail.com>
2017-06-09 21:18:22 +1000

    added basic components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutingModule, routableComponents} from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    routableComponents
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
