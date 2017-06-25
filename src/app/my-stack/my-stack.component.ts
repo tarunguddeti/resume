6d85f78fbd7b8d2da6dc88b083f7d63909b657ff
Tarun <gtarun1991@gmail.com>
2017-06-25 17:58:15 +1000

    added my-stack.component

---
+++ src/app/my-stack/my-stack.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-stack',
+  templateUrl: './my-stack.component.html',
+  styleUrls: ['./my-stack.component.css']
+})
+export class MyStackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
